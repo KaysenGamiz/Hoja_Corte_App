0c37e1d0b945fcd95411577189de687fbbd36c9e
KaysenGamiz <k.aiseen@hotmail.com>
2023-06-18 14:00:28 -0600

    [JS] Archivos para rutas generados y conectados

---
+++ app/controllers/router.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const path = require('path');
+const router = express.Router();
+
+// Router que nos lleva a corterRoute.
+
+const corteRouter = require(path.join(__dirname, '../routes/', 'corteRoute.js'));
+
+router.use('/corte', corteRouter);
