124206ca659a90824c36088bf183b4a52c70ced4
KaysenGamiz <k.aiseen@hotmail.com>
2023-06-23 13:24:57 -0600

    [MODAL] Minor changes in modal, color de linea y cierre de modal

--- app/public/scripts/modal.js
+++ app/public/scripts/modal.js
@@ -1,9 +1,9 @@
 document.addEventListener('DOMContentLoaded', function(){
     var modalBg = document.querySelector('.modal-bg');
-    var modalClose = document.querySelector('.modal-close');
+    var modalSend = document.querySelector('.modal-send');
     modalBg.classList.add('bg-active');
 
-    modalClose.addEventListener('click', function() {
+    modalSend.addEventListener('click', function() {
         modalBg.classList.remove('bg-active');
     });
 
